42cf8737727591277d450ce7fe7e0775beed2913
Gai Radzi <gai@rebooto.io>
2022-03-07 18:01:03 +0200

    Add function to set the highlighted row
    Allow setting which row will be highlighted by
    passing its index

--- table/update.go
+++ table/update.go
@@ -5,6 +5,20 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 )
 
+func (m *Model) SetHighlightRow(index int) {
+	m.rowCursorIndex = index
+
+	if m.rowCursorIndex < 0 {
+		m.rowCursorIndex = 0
+	}
+
+	if m.rowCursorIndex >= len(m.GetVisibleRows()) {
+		m.rowCursorIndex = len(m.GetVisibleRows()) - 1
+	}
+
+	m.currentPage = m.expectedPageForRowIndex(m.rowCursorIndex)
+}
+
 func (m *Model) moveHighlightUp() {
 	m.rowCursorIndex--
 
