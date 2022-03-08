9c63372680890388c966d59791faa190df9ec2e0
Brandon Fulljames <bfullj@gmail.com>
2022-03-08 21:46:54 +0900

    A bit of extra test coverage

--- table/options_test.go
+++ table/options_test.go
@@ -24,3 +24,41 @@ func TestWithHighlightedRowSet(t *testing.T) {
 
 	assert.Equal(t, model.rows[highlightedIndex], model.HighlightedRow())
 }
+
+func TestWithHighlightedRowSetNegative(t *testing.T) {
+	highlightedIndex := -1
+
+	cols := []Column{
+		NewColumn("id", "ID", 3),
+	}
+
+	model := New(cols).WithRows([]Row{
+		NewRow(RowData{
+			"id": "first",
+		}),
+		NewRow(RowData{
+			"id": "second",
+		}),
+	}).WithHighlightedRow(highlightedIndex)
+
+	assert.Equal(t, model.rows[0], model.HighlightedRow())
+}
+
+func TestWithHighlightedRowSetTooHigh(t *testing.T) {
+	highlightedIndex := 2
+
+	cols := []Column{
+		NewColumn("id", "ID", 3),
+	}
+
+	model := New(cols).WithRows([]Row{
+		NewRow(RowData{
+			"id": "first",
+		}),
+		NewRow(RowData{
+			"id": "second",
+		}),
+	}).WithHighlightedRow(highlightedIndex)
+
+	assert.Equal(t, model.rows[1], model.HighlightedRow())
+}
