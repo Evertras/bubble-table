adf136ac1d4925a1f374315163b030118bef0c29
Gai Radzi <gai@rebooto.io>
2022-03-13 13:52:33 +0200

    Fix table layout for multi-line values
    If one of the rows has a multi-line value we want
    to truncate it and display one the first line

--- table/strlimit.go
+++ table/strlimit.go
@@ -1,13 +1,27 @@
 package table
 
-import "github.com/mattn/go-runewidth"
+import (
+	"strings"
+
+	"github.com/mattn/go-runewidth"
+)
 
 func limitStr(str string, maxLen int) string {
 	if maxLen == 0 {
 		return ""
 	}
+
+	newLineIndex := strings.Index(str, "\n")
+	if newLineIndex > -1 {
+		str = str[:newLineIndex]
+	}
+
 	if runewidth.StringWidth(str) > maxLen {
 		return runewidth.Truncate(str, maxLen-1, "") + "…"
+	}
+
+	if newLineIndex > -1 {
+		return str + "…"
 	}
 
 	return str
