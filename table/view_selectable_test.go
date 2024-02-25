efcf93be49a88fa67a9d999d214ac3675b9b9601
Brandon Fulljames <bfullj@gmail.com>
2024-02-25 20:09:36 +0900

    Add test to reproduce bug

--- table/view_selectable_test.go
+++ table/view_selectable_test.go
@@ -121,3 +121,44 @@ func TestSimple3x3WithCustomSelectableTextAndFooter(t *testing.T) {
 
 	assert.Equal(t, expectedTable, rendered)
 }
+
+func TestRegeneratingColumnsKeepsSelectableText(t *testing.T) {
+	columns := []Column{
+		NewColumn("1", "1", 4),
+		NewColumn("2", "2", 4),
+		NewColumn("3", "3", 4),
+	}
+
+	model := New(columns)
+
+	rows := []Row{}
+
+	for rowIndex := 1; rowIndex <= 3; rowIndex++ {
+		rowData := RowData{}
+
+		for columnIndex := 1; columnIndex <= 3; columnIndex++ {
+			id := fmt.Sprintf("%d", columnIndex)
+
+			rowData[id] = fmt.Sprintf("%d,%d", columnIndex, rowIndex)
+		}
+
+		rows = append(rows, NewRow(rowData))
+	}
+
+	model = model.WithRows(rows).
+		SelectableRows(true).
+		WithSelectedText(" ", "✓").
+		WithColumns(columns)
+
+	const expectedTable = `┏━┳━━━━┳━━━━┳━━━━┓
+┃✓┃   1┃   2┃   3┃
+┣━╋━━━━╋━━━━╋━━━━┫
+┃ ┃ 1,1┃ 2,1┃ 3,1┃
+┃ ┃ 1,2┃ 2,2┃ 3,2┃
+┃ ┃ 1,3┃ 2,3┃ 3,3┃
+┗━┻━━━━┻━━━━┻━━━━┛`
+
+	rendered := model.View()
+
+	assert.Equal(t, expectedTable, rendered)
+}
