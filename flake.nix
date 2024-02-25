53205ee5ae8679832b221840d633743d1c3e2e0a
Brandon Fulljames <bfullj@gmail.com>
2024-02-25 11:04:43 +0000

    Nix flake for dev shell (#164)
    * Add nix flake for go itself
    
    * Simplify

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            # Dev tools
+            go
+          ];
+        };
+      });
+}
