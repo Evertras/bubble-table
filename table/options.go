3f92294dce6b5dd4754910eacca43c1502622610
Brandon Fulljames <bfullj@gmail.com>
2025-09-04 19:47:12 -0700

    Add global metadata for style func input as a starting point

--- table/options.go
+++ table/options.go
@@ -499,3 +499,12 @@ func (m Model) WithAdditionalFullHelpKeys(keys []key.Binding) Model {
 
 	return m
 }
+
+// WithGlobalMetadata applies the given metadata to the table. This metadata is passed to
+// some functions such as FilterFuncInput and StyleFuncInput to enable more advanced decisions,
+// such as setting some global theme variable to reference, etc.
+func (m Model) WithGlobalMetadata(metadata map[string]any) Model {
+	m.metadata = metadata
+
+	return m
+}
